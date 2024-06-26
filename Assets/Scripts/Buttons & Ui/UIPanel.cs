0a58bc449e0b0fec66bcd24d79cbcfa8bfe2e488
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-06-26 22:41:27 +0200

    menu

--- Assets/Scripts/Buttons & Ui/UIPanel.cs
+++ Assets/Scripts/Buttons & Ui/UIPanel.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIPanel : MonoBehaviour
 {
+    public Button primaryButton;
+
+
+    private void Start()
+    {
+        primaryButton.Select();
+    }
 
 
 }
